77c4c1e5237b6149fdb3681eb20eb56c1903edd0
Travis Pritchard <pritchard.travis@deq.state.or.us>
2020-12-21 14:30:28 -0800

    Initial commit

---
+++ Tools/data gather.R
@@ -0,0 +1,338 @@
+library(tidyverse)
+library(openxlsx)
+
+
+excursions_conv <- function(n){
+  
+  x = ifelse(n <= 11, 2, qbinom(0.90, n, 0.10, lower.tail = TRUE)+1 )
+  return(x)
+}
+
+make_char <- function(df){
+  df_2 <- df %>%
+    mutate(count_excursion = as.character(count_excursion),
+           Pollu_ID = as.character(Pollu_ID)) 
+  
+  
+}
+
+rename_param <- function(df){
+  df2 <- df %>%
+    mutate(Pollu_ID = as.character(Pollu_ID)) %>%
+    left_join(Pollutants, by = "Pollu_ID") %>%
+    mutate(Char_Name = ifelse(!is.na(Pollutant_DEQ.WQS), Pollutant_DEQ.WQS, Char_Name )) %>%
+    select(AU_ID, MLocID, Pollu_ID, Char_Name, count_excursion )
+}
+
+con <- DBI::dbConnect(odbc::odbc(), "IR 2018")
+
+
+
+Pollutants <- DBI::dbReadTable(con, 'LU_Pollutant') %>%
+  mutate(Pollu_ID = as.character(Pollu_ID)) %>%
+  select(-SSMA_TimeStamp) %>%
+  mutate(Pollutant_DEQ.WQS = trimws(Pollutant_DEQ.WQS, which = "right"))
+
+load("E:/Documents/2018-2020_IR_Database/data/IR_data.Rdata")
+
+
+z_DO_cont_spawn <- DO_cont_spawn %>%
+  mutate(abs_min = ifelse(IRResultNWQSunit < 9, 1, 0)) %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,Excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(Excursion)) %>%
+  mutate(Period = "Spawn") %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,Period, count_excursion ) %>%
+  make_char()
+
+
+z_DO_cont_yearround <- DO_cont_yearround %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,Excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(Excursion)) %>%
+  mutate(Period = "Year round") %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name,Period, count_excursion ) %>%
+  make_char()
+
+
+z_DO_estuary_yearround <- DO_estuary_yearround %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(excursion)) %>%
+  mutate(Period = "Year round") %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name,Period, count_excursion ) %>%
+  make_char()
+
+z_DO_inst_yearround <- DO_inst_yearround %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,Excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(Excursion)) %>%
+  mutate(Period = "Year round") %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name,Period, count_excursion ) %>%
+  make_char()
+
+z_DO_instant_spawn <- DO_instant_spawn %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,Excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(Excursion)) %>%
+  mutate(Period = "Spawn") %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name,Period, count_excursion ) %>%
+  make_char()
+
+
+z_temp_year_round <- temp %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,year_round_Excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(year_round_Excursion)) %>%
+  mutate(Period = "Year round") %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name,Period, count_excursion ) %>%
+  make_char()
+
+
+
+
+z_temp_spawning <- temp %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,Spawn_Excursion.1) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(Spawn_Excursion.1)) %>%
+  mutate(Period = "Spawn") %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name,Period, count_excursion ) %>%
+  make_char()
+
+
+z_Tox_AL_Ammonia <- Tox_AL_Ammonia%>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(excursion)) %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name, count_excursion ) %>%
+  make_char() %>%
+  rename_param() %>%
+  mutate(Char_Name = paste0(Char_Name, "- Aquatic Life"))
+z_Tox_AL_CU <- Tox_AL_CU %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(excursion)) %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name, count_excursion ) %>%
+  make_char() %>%
+  rename_param() %>%
+  mutate(Char_Name = paste0(Char_Name, "- Aquatic Life"))
+
+Z_Tox_AL_Hardness_Metals <- Tox_AL_Hardness_Metals %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(excursion)) %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name, count_excursion ) %>%
+  make_char() %>%
+  rename_param() %>%
+  mutate(Char_Name = paste0(Char_Name, "- Aquatic Life"))
+
+z_Tox_AL_Hardness_Metals <- Tox_AL_Hardness_Metals %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(excursion)) %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name, count_excursion ) %>%
+  make_char() %>%
+  rename_param() %>%
+  mutate(Char_Name = paste0(Char_Name, "- Aquatic Life"))
+
+z_Tox_AL_Others <- Tox_AL_Others %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(excursion)) %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name, count_excursion ) %>%
+  make_char() %>%
+  rename_param() %>%
+  mutate(Char_Name = paste0(Char_Name, "- Aquatic Life"))
+
+z_Tox_AL_Penta <- Tox_AL_Penta %>%
+  select(AU_ID, MLocID, Pollu_ID, Char_Name,excursion) %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = sum(excursion)) %>%
+  select(AU_ID,MLocID, Pollu_ID, Char_Name, count_excursion ) %>%
+  make_char() %>%
+  rename_param() %>%
+  mutate(Char_Name = paste0(Char_Name, "- Aquatic Life"))
+
+
+
+# Bacteria ----------------------------------------------------------------
+
+
+z_bacteria_coast_contact <- bacteria_coast_contact %>%
+  group_by(AU_ID) %>%
+  # list out the maxium geometric mean per AU
+  mutate(OWRD_Basin = first(OWRD_Basin), 
+            Max_Geomean = ifelse(!all(is.na(geomean)),max(geomean, na.rm = TRUE),NA),
+            # maximum percentage of results within geomean groups with more 10 samples that are above criteria 
+            max.perc_above_crit_10 =  ifelse(!all(is.na(perc_above_crit_10)),max(perc_above_crit_10, na.rm = TRUE),NA),
+            # maximum percentage of results within geomean groups with more 5 samples that are above criteria 
+            max.n_samples_greater_perc_crit = ifelse(all(is.na(n_samples_greater_perc_crit)), NA, max(n_samples_greater_perc_crit, na.rm= TRUE)),
+            # percent of samples that do not have 5 or more samples in each 90 day period. 
+            #Used to determine if a 90 day geomean is even possible for cat 3 or cat 3b
+            perc.insuff = sum(less_5)/n(),
+            #Maximum value of AU group. Used for 3 or 3b
+            max.value  = max(Result_cen),
+            SS_Crit = max(SS_Crit),
+            Geomean_Crit = max(Geomean_Crit),
+            Perc_Crit = max(Perc_Crit)) %>%
+  mutate(excursion = ifelse(Result_cen > 130, 1,0),
+         IR_method = case_when(!is.na(Max_Geomean) & Max_Geomean > Geomean_Crit ~ "geomean",
+                               !is.na(max.perc_above_crit_10) & max.perc_above_crit_10 > 0.10 ~ "percent",
+                               !is.na(max.n_samples_greater_perc_crit) & max.n_samples_greater_perc_crit >= 2 ~ "percent",
+                               TRUE ~ "Not impaired"
+         )) %>%
+  ungroup() %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = case_when(first(IR_method) == "geomean" ~ "Contributes to geomean",
+                                        first(IR_method) == "percent" ~ as.character(sum(excursion)),
+                                        TRUE ~ "Not Impaired"
+                                        ))%>%
+  make_char()
+
+
+
+z_bacteria_fresh_contact <- bacteria_fresh_contact %>%
+  mutate(excursion = ifelse(Result_cen > SS_Crit, 1,0)) %>%
+  group_by(AU_ID) %>%
+  # list out the maxium geometric mean per AU
+  mutate(OWRD_Basin = first(OWRD_Basin), 
+            Max_Geomean = ifelse(!all(is.na(geomean)),max(geomean, na.rm = TRUE),NA),
+            max.value  = max(Result_cen),
+            num_Samples = as.numeric(n()),
+            num_ss_excursions = as.numeric(sum(Result_cen > SS_Crit)),
+            critical_excursions = excursions_conv(num_Samples),
+            SS_Crit = max(SS_Crit),
+            Geomean_Crit = max(Geomean_Crit)) %>%
+  mutate(IR_method = case_when(!is.na(Max_Geomean) & Max_Geomean > Geomean_Crit ~ "geomean",
+                               num_Samples >= 5 & num_ss_excursions > critical_excursions ~ "percent",
+                               TRUE ~  "Not impaired"
+                               )) %>%
+  ungroup() %>%
+  group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+  summarise(count_excursion = case_when(first(IR_method) == "geomean" ~ "Contributes to geomean",
+                                        first(IR_method) == "percent" ~ as.character(sum(excursion)),
+                                        TRUE ~ "Not Impaired"
+  ))%>%
+  make_char()
+  
+  
+
+  z_bacteria_Shell_harvest <-  bacteria_Shell_harvest %>%
+    mutate(excursion = ifelse(Result_cen > Perc_Crit, 1, 0)) %>%
+    group_by(AU_ID, OWRD_Basin) %>%
+    mutate(num_samples = n(),
+              median = ifelse(num_samples >= 5, median(Result_cen), NA ),
+              num_exceed = sum(perc_exceed),
+              Perc_Crit = first(Perc_Crit),
+              SS_Crit = first(SS_Crit)
+    ) %>%
+    mutate(IR_method = case_when(!is.na(median) & median > SS_Crit ~ "geomean",
+                                 (num_samples >= 10 & num_exceed/num_samples > 0.10) |
+                                   (num_samples >= 5 & num_samples <= 9 &  num_exceed >= 1 ) ~ "percent",
+                                 TRUE ~ "Not Impaired")) %>%
+    ungroup() %>%
+    group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+    summarise(count_excursion = case_when(first(IR_method) == "geomean" ~ "Contributes to median",
+                                          first(IR_method) == "percent" ~ as.character(sum(excursion)),
+                                          TRUE ~ "Not Impaired"
+    ))%>%
+    make_char()
+    
+
+# chl ---------------------------------------------------------------------
+
+z_chl <- chl %>%
+    mutate(excursion = ifelse(Result_cen > Chla_Criteria, 1, 0)) %>%
+    group_by(AU_ID, Chla_Criteria) %>%
+    mutate(OWRD_Basin = first(OWRD_Basin),
+              Char_Name = first(Char_Name),
+              num_samples = as.numeric(n()),
+              num_ss_excursions = as.numeric(sum(Result_cen > Chla_Criteria)),
+              critical_excursions = excursions_conv(n()),
+              max_result = max(Result_cen),
+              max_mo_avg = max(monthaverage),
+              max_3_mo_avg = max(avg.3.mo, na.rm = TRUE)) %>%
+    mutate(max_3_mo_avg = ifelse(is.infinite(max_3_mo_avg), NA, max_3_mo_avg )) %>%
+    mutate(IR_method = case_when(!is.na(max_3_mo_avg) & max_3_mo_avg > Chla_Criteria ~ "Month average")) %>%
+    ungroup() %>%
+    group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+    summarise(count_excursion = case_when(first(IR_method) == "Month average" ~ "Contributes to 3 month average",
+                                          TRUE ~ as.character(sum(excursion))))%>%
+    make_char()
+  
+
+# pH ----------------------------------------------------------------------
+
+z_pH <- pH %>%
+    group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+    summarise(pH_high_excursions = sum(pH_excursion_high),
+              pH_low_excursions = sum(pH_excursion_low)) %>%
+    mutate(count_excursion = paste(pH_high_excursions, "high pH excursions;", pH_low_excursions, "low pH excursions")) %>%
+    select(AU_ID,MLocID, Pollu_ID, Char_Name, count_excursion )%>%
+    make_char()
+    
+  
+
+# tox_HH ------------------------------------------------------------------
+
+  z_Tox_HH <- Tox_HH %>%
+    select(AU_ID, MLocID, Pollu_ID, Char_Name) %>%
+    group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+    summarise(count_excursion = "Contributes to geomean") %>%
+    mutate(Pollu_ID = as.character(Pollu_ID)) %>%
+    left_join(Pollutants, by = "Pollu_ID") %>%
+    mutate(Char_Name = ifelse(!is.na(Pollutant_DEQ.WQS), Pollutant_DEQ.WQS, Char_Name )) %>%
+    mutate(Char_Name = paste0(Char_Name, "- Human Health")) %>%
+    select(AU_ID,MLocID, Pollu_ID, Char_Name, count_excursion )%>%
+    make_char()
+  
+  
+  z_Tox_HH_Hg_tissue <- Tox_HH_Hg_tissue %>%
+    mutate(Pollu_ID = "109") %>%
+    select(AU_ID, MLocID, Pollu_ID, Char_Name) %>%
+    group_by(AU_ID,MLocID, Pollu_ID, Char_Name) %>%
+    summarise(count_excursion = "Contributes to geomean") %>%
+    mutate(Pollu_ID = as.character(Pollu_ID)) %>%
+    left_join(Pollutants, by = "Pollu_ID") %>%
+    mutate(Char_Name = ifelse(!is.na(Pollutant_DEQ.WQS), Pollutant_DEQ.WQS, Char_Name )) %>%
+    mutate(Char_Name = paste0(Char_Name, "- Human Health")) %>%
+    select(AU_ID,MLocID, Pollu_ID, Char_Name, count_excursion )%>%
+    make_char()
+    
+    
+  
+ 
+# Put it together ---------------------------------------------------------
+
+
+all_together <- bind_rows(z_DO_cont_yearround,z_DO_estuary_yearround,
+                          z_DO_inst_yearround, z_DO_instant_spawn,
+                          z_temp_spawning, z_temp_year_round,z_Tox_AL_Ammonia, 
+                          z_Tox_AL_CU, #z_Tox_AL_Hardness_Metals, 
+                          Z_Tox_AL_Hardness_Metals, 
+                          z_Tox_AL_Others, z_bacteria_coast_contact,
+                          z_bacteria_fresh_contact, z_bacteria_Shell_harvest,
+                          z_chl,z_pH, z_Tox_HH,  z_Tox_HH_Hg_tissue)
+
+
+data_wide <- all_together %>%
+  distinct(AU_ID, MLocID,Pollu_ID,Period, .keep_all = TRUE) %>%
+  ungroup() %>%
+  mutate(assessment = ifelse(!is.na(Period), 
+                             paste0(Char_Name, "- ", Period, " excursions"), 
+                             paste(Char_Name, "excursions" ))) %>%
+  select(-Pollu_ID, -Char_Name, -Period) %>%
+  mutate(AU_Type = case_when(grepl("SR", AU_ID) ~ "River and Stream",
+                             grepl("WS", AU_ID) ~ "Watershed",
+                             grepl("LK", AU_ID) ~ "Lake/Reservoir",
+                             grepl("CL", AU_ID) ~ "Coastline",
+                             grepl("OC", AU_ID) ~ "Ocean",
+                             grepl("EB", AU_ID) ~ "Estuary/Bay",
+                             )) %>%
+  pivot_wider(names_from = assessment, values_from = count_excursion) 
+
+save(data_wide, file = "data/wide_data.Rdata")
+
+
+
+
---
+++ app.R
@@ -0,0 +1,197 @@
+#To do
+#   spawning continuous absolte minimum excursions not reflected
+#   Need to check if year round abs minimums need to be factored
+#   Add crosswalk database
+
+
+library(shiny)
+library(dplyr)
+
+
+load("data/wide_data.Rdata")
+AU_s = unique(data_wide$AU_ID)
+param_groups <- c("Aquatic Life Toxics",
+                  "Human Health Toxics",
+                  "Bacteria",
+                  "Chlorophyll",
+                  "Dissolved Oxygen",
+                  'pH',
+                  'Temperature'
+                  )
+
+Bacteria_cols <- c("Enterococcus excursions",
+                   "Escherichia coli excursions",
+                   "Fecal Coliform excursions")
+
+
+DO_cols <-c("Dissolved oxygen (DO)- Year round excursions",
+            "Dissolved oxygen (DO)- Spawn excursions")
+
+Temperature_cols <- c("Temperature, water- Year round excursions","Temperature, water- Spawn excursions" )
+
+Al_cols <- c("Ammonia- Aquatic Life excursions",          
+             "Copper- Aquatic Life excursions"                 , "Cadmium- Aquatic Life excursions"                           ,
+             "Lead- Aquatic Life excursions"                  ,  "Nickel- Aquatic Life excursions"                            ,
+             "Silver- Aquatic Life excursions"                ,  "Zinc- Aquatic Life excursions"                              ,
+             "Arsenic, Inorganic- Aquatic Life excursions"    ,  "Aldrin- Aquatic Life excursions"                            ,
+             "BHC Gamma (Lindane)- Aquatic Life excursions"   ,  "Chlordane- Aquatic Life excursions"                         ,
+             "Chlorpyrifos- Aquatic Life excursions"          ,  "DDD 4,4'- Aquatic Life excursions"                          ,
+             "DDE 4,4'- Aquatic Life excursions"              ,  "DDT 4,4'- Aquatic Life excursions"                          ,
+             "Dieldrin- Aquatic Life excursions"              ,  "Endosulfan Alpha- Aquatic Life excursions"                  ,
+             "Endrin- Aquatic Life excursions"                ,  "Guthion- Aquatic Life excursions"                           ,
+             "Heptachlor- Aquatic Life excursions"            ,  "Heptachlor Epoxide- Aquatic Life excursions"                ,
+             "Malathion- Aquatic Life excursions"             ,  "Methoxychlor- Aquatic Life excursions"                      ,
+             "Mirex- Aquatic Life excursions"                 ,  "Polychlorinated Biphenyls (PCBs)- Aquatic Life excursions"  ,
+             "Endosulfan Beta- Aquatic Life excursions"       ,  "Alkalinity- Aquatic Life excursions"                        ,
+             "Chloride- Aquatic Life excursions"              ,  "Iron (total)- Aquatic Life excursions"                      ,
+             "Parathion- Aquatic Life excursions"             ,  "Mercury (total)- Aquatic Life excursions"                   ,
+             "Chlorine- Aquatic Life excursions"              ,  "Cyanide- Aquatic Life excursions"                           ,
+             "Toxaphene- Aquatic Life excursions" ) 
+
+HH_cols <- c("Antimony- Human Health excursions"                         ,  "Selenium- Human Health excursions"                          ,
+             "Thallium- Human Health excursions"                         ,  "Acenaphthene- Human Health excursions"                      ,
+             "Anthracene- Human Health excursions"                       ,  "Benz(a)anthracene- Human Health excursions"                 ,
+             "Benzo(a)pyrene- Human Health excursions"                   ,  "Benzo(b)fluoranthene 3,4- Human Health excursions"          ,
+             "Benzo(k)fluoranthene- Human Health excursions"             ,  "BHC Alpha- Human Health excursions"                         ,
+             "BHC Beta- Human Health excursions"                         ,  "Butylbenzyl Phthalate- Human Health excursions"             ,
+             "Chloroethyl Ether bis 2- Human Health excursions"          ,  "Chloronaphthalene 2- Human Health excursions"               ,
+             "Chlorophenol 2- Human Health excursions"                   ,  "Chrysene- Human Health excursions"                          ,
+             "Dibenz(a,h)anthracene- Human Health excursions"            ,  "Dichlorobenzene(o) 1,2- Human Health excursions"            ,
+             "Dichlorobenzene(p) 1,4- Human Health excursions"           ,  "Dichlorophenol 2,4- Human Health excursions"                ,
+             "Diethyl Phthalate- Human Health excursions"                ,  "Dimethyl Phthalate- Human Health excursions"                ,
+             "Dimethylphenol 2,4- Human Health excursions"               ,  "Dinitrophenol 2,4- Human Health excursions"                 ,
+             "Dinitrotoluene 2,4- Human Health excursions"               ,  "Dioxin (2,3,7,8-TCDD)- Human Health excursions"             ,
+             "Endosulfan Beta- Human Health excursions"                  ,  "Endosulfan Sulfate- Human Health excursions"                ,
+             "Endrin Aldehyde- Human Health excursions"                  ,  "Fluoranthene- Human Health excursions"                      ,
+             "Fluorene- Human Health excursions"                         ,  "Hexachlorobenzene- Human Health excursions"                 ,
+             "Hexachlorobutadiene- Human Health excursions"              ,  "Hexachlorocyclopentadiene- Human Health excursions"         ,
+             "Hexachloroethane- Human Health excursions"                 ,  "Isophorone- Human Health excursions"                        ,
+             "Manganese- Human Health excursions"                        ,  "Methyl-4,6-dinitrophenol- Human Health excursions"          ,
+             "Nitrobenzene- Human Health excursions"                     ,  "Nitrosodibutylamine, N- Human Health excursions"            ,
+             "Nitrosodiethylamine, N- Human Health excursions"           ,  "Nitrosodimethylamine, N- Human Health excursions"           ,
+             "Nitrosodi-n-propylamine, N- Human Health excursions"       ,  "Nitrosopyrrolidine, N- Human Health excursions"             ,
+             "Pentachlorobenzene- Human Health excursions"               ,  "Pentachlorophenol- Human Health excursions"                 ,
+             "Phenol- Human Health excursions"                           ,  "Pyrene- Human Health excursions"                            ,
+             "Tetrachlorobenzene, 1,2,4,5-- Human Health excursions"     ,  "Trichlorobenzene 1,2,4- Human Health excursions"            ,
+             "Trichlorophenol 2,4,6- Human Health excursions"            ,  "Trichlorophenol, 2, 4, 5-- Human Health excursions"         ,
+             "DDD 4,4'- Human Health excursions"                         ,  "DDT 4,4'- Human Health excursions"                          ,
+             "Endosulfan Alpha- Human Health excursions"                ,   "DDE 4,4'- Human Health excursions"                          ,
+             "Barium- Human Health excursions"                          ,   "Chlorophenoxy Herbicide (2,4,5,TP)- Human Health excursions",
+             "Chlorophenoxy Herbicide (2,4-D)- Human Health excursions" ,   "Benzene- Human Health excursions"                           ,
+             "Bromoform- Human Health excursions"                       ,   "Carbon Tetrachloride- Human Health excursions"              ,
+             "Chlorobenzene- Human Health excursions"                   ,   "Chlorodibromomethane- Human Health excursions"              ,
+             "Chloroform- Human Health excursions"                      ,   "Dichloroethane 1,2- Human Health excursions"                ,
+             "Dichloroethylene 1,1- Human Health excursions"            ,   "Dichloroethylene trans 1,2- Human Health excursions"        ,
+             "Dichloropropane 1,2- Human Health excursions"             ,   "Dichloropropene 1,3- Human Health excursions"               ,
+             "Ethylbenzene- Human Health excursions"                    ,   "Methyl Bromide- Human Health excursions"                    ,
+             "Methylene Chloride- Human Health excursions"              ,   "Tetrachloroethane 1,1,2,2- Human Health excursions"         ,
+             "Tetrachloroethylene- Human Health excursions"             ,   "Toluene- Human Health excursions"                           ,
+             "Trichloroethane 1,1,2- Human Health excursions"           ,   "Trichloroethylene- Human Health excursions"                 ,
+             "Vinyl Chloride- Human Health excursions"                  ,   "Nitrates- Human Health excursions"                          ,
+             "Ethylhexyl Phthalate bis 2- Human Health excursions"      ,   "Benzidine- Human Health excursions"                         ,
+             "Dichlorobenzidine 3,3'- Human Health excursions"          ,   "Acrolein- Human Health excursions"                          ,
+             "Acrylonitrile- Human Health excursions"                   ,   "Dichlorobenzene(m) 1,3- Human Health excursions"            ,
+             "Dichlorobromomethane- Human Health excursions"            ,   "Di-n-butyl Phthalate- Human Health excursions"              ,
+             "Indeno(1,2,3-cd)pyrene- Human Health excursions"          ,   "Nitrosodiphenylamine, N- Human Health excursions"           ,
+             "Nickel- Human Health excursions"                          ,   "Zinc- Human Health excursions"                              ,
+             "Methylmercury- Human Health excursions" )
+
+# populate values in selectize boxes
+#get a vector of unique AUs
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Monitoring Location Checker - DRAFT: NOT COMPLETE"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            actionButton("go", "Filter",  icon("filter")),
+            selectizeInput("AUs",
+                           "Select Assessment Unit",
+                           choices = AU_s,
+                           multiple = TRUE,
+                           options = list(maxOptions = 7000)),
+            selectizeInput("param_groups",
+                           "Select Parameter Group",
+                           choices = param_groups,
+                           multiple = TRUE,
+                           options = list(maxOptions = 7000))
+            
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            tableOutput('table')
+        )
+    )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    
+    table_Data <- eventReactive(input$go,{
+        
+        t <- data_wide %>%
+            filter(AU_ID %in% input$AUs)
+        
+        
+        if(!is.null(input$param_groups)){
+        select_vectors <- c("AU_ID", "AU_Type", "MLocID")
+
+
+        if("Bacteria" %in% input$param_groups) {
+            select_vectors <- c(select_vectors, Bacteria_cols)
+
+        }
+
+        if("Temperature" %in% input$param_groups){
+            select_vectors <- c(select_vectors, Temperature_cols)
+        }
+
+        if("Aquatic Life Toxics" %in% input$param_groups){
+            select_vectors <- c(select_vectors, Al_cols)
+        }
+
+        if("Human Health Toxics" %in% input$param_groups){
+            select_vectors <- c(select_vectors, HH_cols)
+        }
+
+
+        if("Chlorophyll" %in% input$param_groups){
+            select_vectors <- c(select_vectors, "Chlorophyll a excursions")
+        }
+
+        if("Dissolved Oxygen" %in% input$param_groups){
+            select_vectors <- c(select_vectors, DO_cols)
+        }
+        if("pH" %in% input$param_groups){
+            select_vectors <- c(select_vectors, "pH excursions")
+        }
+
+
+        t <- t %>%
+            select(select_vectors)
+
+
+
+             }
+        
+        return(t)
+    })
+    
+    
+    output$table <- renderTable(table_Data(),
+                                bordered = TRUE,  
+                                align = 'c',  
+                                na = 'Unassessed'
+    )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
